1e654999c91dad47f0d6a57c9cb7be34e9898a14
Ibrahim Fikry <ibrahim.fikry2050@gmail.com>
2024-05-18 13:56:44 +0300

    Add Base Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ListRecipesComponent } from './recipes/list-recipes/list-recipes.component';
+import { ItemRecipeComponent } from './recipes/list-recipes/item-recipe/item-recipe.component';
+import { DetailsRecipeComponent } from './recipes/details-recipe/details-recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditeComponent } from './shopping-list/shopping-edite/shopping-edite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ListRecipesComponent,
+    ItemRecipeComponent,
+    DetailsRecipeComponent,
+    ShoppingListComponent,
+    ShoppingEditeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/recipes/list-recipes/item-recipe/item-recipe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-recipe',
+  templateUrl: './item-recipe.component.html',
+  styleUrl: './item-recipe.component.scss'
+})
+export class ItemRecipeComponent {
+
+}
