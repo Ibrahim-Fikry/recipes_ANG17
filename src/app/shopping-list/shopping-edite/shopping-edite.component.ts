3c740fb105e08a588122bca88c5593ab3c219f77
Ibrahim Fikry <ibrahim.fikry2050@gmail.com>
2024-05-25 21:28:15 +0300

    shopping edit component

--- src/app/shopping-list/shopping-edite/shopping-edite.component.ts
+++ src/app/shopping-list/shopping-edite/shopping-edite.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingrediants } from '../../../Shared/Ingrediants';
 
 @Component({
   selector: 'app-shopping-edite',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './shopping-edite.component.scss'
 })
 export class ShoppingEditeComponent {
-
+  @ViewChild ("nameinpt",{static:true}) nameref?:ElementRef;
+  @ViewChild("countinpt",{}) countref?:ElementRef;
+  @Output() ingrediantadded =new EventEmitter<Ingrediants>();
+  
+  Adding(){
+    const item = new Ingrediants( this.nameref?.nativeElement.value,this.countref?.nativeElement.value);
+    this.ingrediantadded?.emit(item);
+                           
+  }
+  
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,19 +7,12 @@ import { Ingrediants } from '../../Shared/Ingrediants';
   styleUrl: './shopping-list.component.scss'
 })
 export class ShoppingListComponent {
-@ViewChild ("nameinpt",{static:true}) nameref?:ElementRef;
-@ViewChild("countinpt",{}) countref?:ElementRef;
-@Output() ingrediantadded? =new EventEmitter<Ingrediants>();
 IngrediantsList:Ingrediants[]=[
   new Ingrediants('Apple',100),
   new Ingrediants('Pizza',150),
   new Ingrediants('Mango',60),
 ]
-Adding(){
-  const item = new Ingrediants( this.nameref?.nativeElement.value,this.countref?.nativeElement.value);
-  debugger 
-  this.IngrediantsList.push(item);                            
-  
-
+addtolist(item:Ingrediants){
+  this.IngrediantsList.push(item)
 }
 }
