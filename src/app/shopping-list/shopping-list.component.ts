92c4c937e4c4ce8c52adda21c833268320bfd5df
Ibrahim Fikry <ibrahim.fikry2050@gmail.com>
2024-05-25 20:57:49 +0300

    add ingrediant to shopping list (view child-template refrence variable

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
 import { Ingrediants } from '../../Shared/Ingrediants';
 
 @Component({
@@ -7,9 +7,19 @@ import { Ingrediants } from '../../Shared/Ingrediants';
   styleUrl: './shopping-list.component.scss'
 })
 export class ShoppingListComponent {
+@ViewChild ("nameinpt",{static:true}) nameref?:ElementRef;
+@ViewChild("countinpt",{}) countref?:ElementRef;
+@Output() ingrediantadded? =new EventEmitter<Ingrediants>();
 IngrediantsList:Ingrediants[]=[
   new Ingrediants('Apple',100),
   new Ingrediants('Pizza',150),
   new Ingrediants('Mango',60),
 ]
+Adding(){
+  const item = new Ingrediants( this.nameref?.nativeElement.value,this.countref?.nativeElement.value);
+  debugger 
+  this.IngrediantsList.push(item);                            
+  
+
+}
 }
